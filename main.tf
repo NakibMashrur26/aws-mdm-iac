3e7ea65d35866f13a7318293385701f02b42d6fb
Nakib Mashrur <nmashrur@Nakibs-MacBook-Air.local>
2026-04-25 15:17:30 -0400

    added basic module

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+module "munki_repo" {
+  source = "./modules/s3"
+
+  bucket_name = "munki-repo"
+  environment = "dev"
+
+  tags = {
+    Project = "macos-iac"
+    Owner   = "nakib"
+  }
+}
